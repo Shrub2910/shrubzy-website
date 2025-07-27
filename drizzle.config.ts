18b04a88532c6b7c7f3aae2b00947759011e6024
Shrub2910 <isaacmcallister2006@gmail.com>
2025-07-28 00:04:29 +0100

    fixed cert issues

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
     schema: './server/database/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
-        ssl: 'require',
+        host: process.env.POSTGRES_HOST!,
+        port: 5432,
+        user: process.env.POSTGRES_USER!,
+        password: process.env.POSTGRES_PASSWORD!,
+        database: process.env.POSTGRES_DATABASE!,
+        ssl: {ca: process.env.SUPABASE_CA_CERT!},
+        
     },
 })
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -3,11 +3,16 @@ import {drizzle} from 'drizzle-orm/node-postgres'
 import {Pool} from "pg"
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  host: process.env.POSTGRES_HOST!,
+  port: 5432,
+  user: process.env.POSTGRES_USER!,
+  password: process.env.POSTGRES_PASSWORD!,
+  database: process.env.POSTGRES_DATABASE!,
   ssl: {
     rejectUnauthorized: true,
     ca: process.env.SUPABASE_CA_CERT
   },
+
 });
 
 const db = drizzle({client: pool})
