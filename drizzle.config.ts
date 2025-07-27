bf17e8f95a2a0aee343717d9856b1d973884cfbe
Shrub2910 <isaacmcallister2006@gmail.com>
2025-07-28 00:37:49 +0100

    added port to environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         host: process.env.DATABASE_HOST!,
-        port: 5432,
+        port: parseInt(process.env.DATABASE_PORT!),
         user: process.env.DATABASE_USER!,
         password: process.env.DATABASE_PASSWORD!,
         database: process.env.DATABASE_DATABASE!,
--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -4,7 +4,7 @@ import {Pool} from "pg"
 
 const pool = new Pool({
   host: process.env.DATABASE_HOST!,
-  port: 5432,
+  port: parseInt(process.env.DATBASE_PORT!),
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_DATABASE!,
